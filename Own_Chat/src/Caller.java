37a543032841ad39256dbac666fb57def1e74d3b
katebudyanskaya <budyanskaya96@mail.ru>
2015-11-03 01:16:56 +0200

    Kate Caller

---
+++ Own_Chat/src/Caller.java
@@ -0,0 +1,77 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import Command.Command_Type;
+
+
+public class Caller {
+	private String localNick;
+	private SocketAddress remoteAddress;
+	private String ip;
+	private boolean status;
+	private Socket s;
+	private Connection call;
+	 private final byte code;
+	 
+	public Caller(){
+		
+	}
+
+	public Caller(String localNick){
+		this.localNick=localNick;
+	}
+	public Caller(String localNick, SocketAddress remoteAddress){
+		this.localNick=localNick;
+		this.remoteAddress=remoteAddress;
+	}
+
+	public Caller(String localNick,String ip){
+		this.localNick=localNick;
+		this.ip=ip;
+	}
+	
+	//”станавливает исход€щее соединение. ¬озвращает результат только после удаленного подтверждени€ (или неподтверждени€) беседы. ѕодробный результат звонка доступен в поле status.
+	
+	public Connection call() throws IOException{
+		s.connect(new InetSocketAddress(ip, 28411));
+		if (status == false) {
+				return s;}
+	}
+	
+	public String getLocalNick(){
+		return localNick;
+	}
+	
+	public SocketAddress getRemoteAddress(){
+		return remoteAddress;
+	}
+	
+	public String getRemoteNick(){
+	
+	}
+	
+	public Caller.CallStatus getStatus(){
+		
+	}
+	
+	public void setLocalNick(String localNick){
+		
+	}
+	
+	public void setRemoteAddress(SocketAddress remoteAddress){
+		
+	}
+	
+	protected Caller (byte callStatusIndex) {
+	    code = callStatusIndex;
+	  }
+	 private static enum CallStatus{BUSY, NO_SERVICE, NOT_ACCESSIBLE, OK, REJECTED};
+	 public static Caller getCall(String text) {
+		    for (CallStatus cs: CallStatus.values())
+		      if (text.equals(cs.name()))
+			return new Caller((byte) cs.ordinal());
+		    return null;
+		  }
+	
+}
---
+++ Own_Chat/src/Caller/CallStatus.java
@@ -0,0 +1,5 @@
+package Caller;
+
+public class CallStatus {
+
+}
