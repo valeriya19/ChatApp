a6a11b39a23ea0fe75a7922c796d7c39118cb5b5
valeriya19 <ivvsh5@gmail.com>
2015-11-11 00:38:13 +0300

    гуи
    вот и гуи

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main{
+    public static ChatForm form= new ChatForm();
+    public static FriendsForm friendsForm =new FriendsForm();
+    public static void main(String[] args) {
+
+        form.setVisible(true);
+    }
+}
+
