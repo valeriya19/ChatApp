79446f2bf94bf08a072312673cd9ceb5ff7e389d
valeriya19 <ivvsh5@gmail.com>
2015-11-16 11:28:20 +0300

    GUI

---
+++ src/FriendsForm.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.lang.reflect.Array;
+
+/**
+ * Created by Mac on 10.11.2015.
+ */
+public class FriendsForm extends JFrame {
+
+    private JPanel rootPanel;
+
+    public FriendsForm() {
+        super();
+        setContentPane(rootPanel);
+        setSize(600,500);
+    }
+}
