060e361b503298019e2076957a68bb59f17ce08b
M-Sh-97 <m-sh-97@yandex.ua>
2015-12-12 19:37:40 +0200

    CallListener 1.3.0.

--- src/CallListener.java
+++ src/CallListener.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
@@ -21,8 +22,8 @@ class CallListener {
     }
 
     public CallListener(String localNick, String localIP) throws IOException {
-        ss = new ServerSocket(Protocol.port);
-//		ss.bind(new InetSocketAddress(localIP, Protocol.port));
+        ss = new ServerSocket();
+	ss.bind(new InetSocketAddress(localIP, Protocol.port));
         this.localNick = localNick;
         this.listenAddress = ss.getLocalSocketAddress();
     }
